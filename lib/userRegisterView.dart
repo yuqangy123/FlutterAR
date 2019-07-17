b007af30a74ba69cbb10bd024d6912c9c545708a
yuqangy@qq.com <yuqangy@qq.com>
2019-07-17 23:57:50 +0800

    1

--- lib/userRegisterView.dart
+++ lib/userRegisterView.dart
@@ -12,7 +12,7 @@ class _userRegitserViewerState extends State<userRegisterView> {
   Widget build(BuildContext context) {
     // TODO: implement build
     final screenSize = MediaQuery.of(context).size;
-    final containerSize = Size(260.0, 500.0);
+    final containerSize = Size(300.0, 500.0);
     return Offstage(offstage:false,//offstage控制child是否无效
       child:
       Stack(
@@ -41,37 +41,69 @@ class _userRegitserViewerState extends State<userRegisterView> {
           ),
           Center(
             child: Container(
-              padding: const EdgeInsets.only(top:15.0, left: 15),
+              padding: const EdgeInsets.only(top:15.0),
               alignment: Alignment.topLeft,//子控件靠左上角
-              width: containerSize.width,
-              height: containerSize.height,
+              width: containerSize.width-50,
+              height: containerSize.height-320,//布局控件的整块大小
 
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("手机号", style:TextStyle(color: Colors.white, fontSize: 16)),
-                      new TextField(
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle( fontSize: 14, color: Colors.white,),
-                        ),
-                      )
-                    ]
+                  Container(
+                    height: 70,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("手机号", style:TextStyle(color: Colors.white, fontSize: 16)),
+                          TextField(
+                            keyboardType: TextInputType.phone,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              helperStyle: TextStyle(fontSize: 40),
+                              //border: InputBorder.none,
+                              labelStyle: TextStyle( fontSize: 40, color: Colors.white,),
+                            ),
+                          )
+                        ]
+                    ),
                   ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text("验证码", style:TextStyle(color: Colors.white, fontSize: 16)),
-                        new TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelStyle: TextStyle( fontSize: 14, color: Colors.white),
+
+                  Container(
+                    height: 70,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text("验证码", style:TextStyle(color: Colors.white, fontSize: 16)),
+                          Stack(
+                            alignment : AlignmentDirectional(0.98, 0.0),
+                            children: <Widget>[
+                            TextField(
+                              keyboardType: TextInputType.phone,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  helperStyle: TextStyle(fontSize: 40),
+                                  labelStyle: TextStyle( fontSize: 40, color: Colors.white,),
+                                ),
+                              ),
+                            SizedBox(
+                              width: 82.0,
+                              height: 34,
+                              child: RaisedButton(
+                                color: Colors.deepPurpleAccent,
+                                textColor: Colors.amber,
+                                child: Text('获取验证码', style:TextStyle(fontSize: 10)),
+                                onPressed: (){},
+                              ),
+                            ),
+                            ],
                           ),
-                        )
-                      ]
-                  )
+                        ]
+                    ),
+                  ),
                 ],
               ),
             )
